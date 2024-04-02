4aec5aa863bd5f1dea976f3a63927aca4e08f013
Nelson, Scott <snelson@collegeboard.org>
2024-04-02 09:35:01 -0700

    added documents to the .swm folder

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ const createWindow = () => {
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
-  }
+  };
 
   mainWindow = new MainWindow(options);
 
@@ -47,7 +47,7 @@ const createWindow = () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  if(process.platform === "darwin"){
+  if (process.platform === "darwin") {
     app.dock.hide();
   }
 
